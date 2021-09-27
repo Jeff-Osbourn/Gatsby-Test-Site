06bb337a6561e23cbfc985366686aa68370e1c2a
Jeff Osbourn <PippyPappy@gmail.com>
2021-09-27 15:00:57 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,3 +14,18 @@ const IndexPage = () => {
 
 // Step 3: Export your component
 export default IndexPage
+
+import { Link } from 'gatsby'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+
+export default IndexPage
