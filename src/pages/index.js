121a4069a1f6631de174c29dda130b5538c6d26e
Jeff Osbourn <PippyPappy@gmail.com>
2021-09-27 15:14:23 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
@@ -14,17 +15,3 @@ export default IndexPage
 // Step 3: Export your component
 export default IndexPage
 
-import { Link } from 'gatsby'
-
-const IndexPage = () => {
-  return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
-}
-
-export default IndexPage
